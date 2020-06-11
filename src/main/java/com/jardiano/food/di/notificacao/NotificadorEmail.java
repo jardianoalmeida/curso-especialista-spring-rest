3285b1a689ae198a390d145d4f7d3ef6ce135aa0
jardiano <jardiano.almeida@gmail.com>
2020-06-11 13:17:15 -0300

    Definindo beans com @Component

---
+++ src/main/java/com/jardiano/food/di/notificacao/NotificadorEmail.java
@@ -0,0 +1,14 @@
+package com.jardiano.food.di.notificacao;
+
+import com.jardiano.food.di.modelo.Cliente;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificadorEmail {
+
+    public void notificar(Cliente cliente, String mensagem) {
+        System.out.printf("Notificando %s através do e-mail %s: %s\n",
+                cliente.getNome(), cliente.getEmail(), mensagem);
+    }
+
+}
---
+++ src/main/java/com/jardiano/food/di/service/AtivacaoClienteService.java
@@ -0,0 +1,18 @@
+package com.jardiano.food.di.service;
+
+import com.jardiano.food.di.modelo.Cliente;
+import com.jardiano.food.di.notificacao.NotificadorEmail;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AtivacaoClienteService {
+
+    private NotificadorEmail notificador;
+
+    public void ativar(Cliente cliente) {
+        cliente.ativar();
+
+        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+    }
+
+}
